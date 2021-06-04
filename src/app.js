d734a9ca00913d48301de9e5a39c15af99bfae53
FedericoManzano <federicomanzano111@gmail.com>
2021-06-04 14:14:22 -0300

    Nav terminada

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("compila")
+
+import Nav from "./moduls/Nav"
+
+(function() {
+
+    const navInit = () => {
+        Nav.init()
+    }
+
+
+    const BS = {
+        NavInit: () => navInit()
+    } 
+    
+
+    window.BS = BS
+})()
+
+export default BS
---
+++ src/moduls/Nav.js
@@ -0,0 +1,94 @@
+import $ from "jquery"
+
+/**
+ * Logica del Nav de la librería 
+ * Muestra o oculta el sidebar dependiendo el estado de mismo
+ * en un momento determinado según la demanda del usuario.
+ */
+(function(){
+
+    // Desplazamiento del sidenav hacia la izquierda
+    const HIDE      = -210
+
+    // Desplazamiento del sidenav hacia la derecha
+    const SHOW      = 0
+
+    // Visible none
+    let visible     = false
+
+    // ID del sidenav
+    let id          = null
+
+    /**
+     * Inicializa los elementos del Nav
+     * Agrega el cover para desaparecer el nav
+     */
+    const initNav = () => {
+        // Agrega el sidebar cover
+        $("body").append(`<div class="body-sidebar-cover"></div>`)
+
+        // Oculta el sidebar cover
+        $(".body-sidebar-cover").hide()
+
+        // Obtiene el id del sidebar
+        id = $(".bs-trigger").data("trigger")
+    }
+
+    /**
+     * Oculta o muestra el sidebar dependiendo 
+     * del valor de la variable (visible)
+     * @param {desplazamiento del sidebar} left 
+     */
+    const move = (left) => {
+        if(visible) {
+            $(id).css("left", left)
+            $(".body-sidebar-cover").hide()
+            visible = false
+        } else {
+            $(id).css("left", left)
+            $(".body-sidebar-cover").show()
+            visible = true
+        }
+    }
+
+    /**
+     * Captura el evento click sobre el icono del menú
+     * Si visible = false llama a move para mostrar el 
+     * sidebar
+     * @see move()
+     */
+    const activate = () => {
+        $(".bs-trigger").on("click", (e) => {
+            // Si id existe y es diferente a undefined y visible = false
+            if( id !== null && id !== undefined && !visible ) 
+                move(SHOW)
+        })
+    }
+
+
+    /**
+     * Captura el evento click sobre el icono del menú
+     * Si visible = true llama a move para ocultar el 
+     * sidebar
+     * @see move()
+     */
+    const desactivate = () => {
+        $(".body-sidebar-cover").on("click", (e) => {
+            if( id !== null && id !== undefined && visible ) 
+                move(HIDE)
+        })
+    }
+
+
+    const Nav = {
+        init: () => {
+            initNav()       // Inicializa el nav
+            activate()      // activa el nav si este no es visible
+            desactivate()   // desactiva a el nav si está activado
+        }
+    }
+
+    window.Nav = Nav
+})()
+
+export default Nav
