459569b13e125b050ba9888295c0f1a36100f864
FedericoManzano <federicomanzano111@gmail.com>
2021-06-07 01:22:27 -0300

    Primera aproimacion a GIT

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-     // { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   mode: "development"
