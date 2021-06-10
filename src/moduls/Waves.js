56080d992e39b9e726809aab4d40afd14316b112
Federico <FedericoManzano>
2021-06-10 15:28:25 -0300

    Efecto waves arreglado

--- src/moduls/Waves.js
+++ src/moduls/Waves.js
@@ -6,26 +6,42 @@ import $ from "jquery"
  */
 (function() {
 
+
+    // Clase de css con los estilos de objeto temporal
+    let claseEfecto = "waves-effect"
+
+    // Objeto temporal a añadir durante el efecto
+    let objetoTemporal = `<span class=${claseEfecto}></span>`
+
     const inicializar = () => {
         $(".waves").on("click", (e) => {
 
             // Color seleccionado por el usuario 
             // recibe un valor hexadecimal
             let color = $(e.target).data("color")
+
+
+            // Si el usuario no añade el data-color 
+            // lo inicializo con blanco
+            if(color === null || color === undefined) 
+                color = "#fff"
             
-            let elemento = $(`<span class="waves-effect"></span>`)
+            let elemento = $(objetoTemporal)
             let ancho = $(e.target).innerWidth()
             let alto = $(e.target).innerHeight()
 
+            // Configurar el color de fondo del elemento a agregar
             $(elemento).css("background-color", color)
-        
+            /*
+                Coordenadas de donde raliza click el usuario
+            */
             let posX = e.pageX - $(e.target).offset().left
             let posY = e.pageY - $(e.target).offset().top
 
             $(elemento).css({
-                width:      0,
+                width:      0, // Comienza con un ancho de 0
                 height:     0,
-                left:       posX,
+                left:       posX, // se hubica en la posición del click
                 top:        posY,
             }).appendTo(e.target).animate({
                 width:      ancho > alto ? ancho: alto,
