e7306c40181fb0fab2aa3c773a781d2d354f8834
Federico <FedericoManzano>
2021-06-30 01:49:19 -0300

    Refactorizacion de codigo y documentacion

--- src/moduls/CommentDim.js
+++ src/moduls/CommentDim.js
@@ -1,15 +1,25 @@
 import Comment from "dynamics-tips/src/moduls/Comment"
 
+/**
+ * Comentarios dinámicos de la dependecia Dynamics Tips
+ * @see node_modules/dynamics-tips/src/moduls/CommentDim
+ */
 (function () {
 
-   
+    
     const CommentDin = {
-        iniciar: () => {
-            Comment.init()
-        },
-        destroy: () => {
-            Comment.destroy()
-        }
+        /**
+         * Iniciar con el metodo estático 
+         * de la clase Comment
+         * @see node_modules/dynamics-tips/moduls/Comment.init()
+         */
+        iniciar: () => Comment.init(),
+
+        /**
+         * Destruye y desvincula todos los eventos asociados 
+         * a los elementos. 
+         */
+        destroy: () => Comment.destroy()
     }
 
     window.CommentDin = CommentDin
